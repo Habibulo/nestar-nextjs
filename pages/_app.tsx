85526be3b8d371b2b0c20b8bab9f0e1ef80ef93e
Habibullo <hhalimjonov1997@gmail.com>
2024-09-17 03:03:19 +0900

    feat: MUI customized integration

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,20 @@
-import "@/styles/globals.css";
 import type { AppProps } from "next/app";
+import { ThemeProvider, createTheme } from "@mui/material";
+import { CssBaseline } from "@mui/material";
+import { light } from "../scss/MaterialTheme";
+import { useState } from "react";
+
 
 export default function App({ Component, pageProps }: AppProps) {
   console.log("App");
-  return <Component {...pageProps} />;
+  // @ts-ignore
+  const [theme, setTheme] = useState(createTheme(light));
+
+  // Socket.oi
+  return (
+    <ThemeProvider theme={theme}>
+      <CssBaseline/>
+      <Component {...pageProps} />
+    </ThemeProvider>);
 }
+
