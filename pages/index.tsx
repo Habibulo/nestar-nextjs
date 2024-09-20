2869215d3c6a20236089a475e8586c6975a1b4d4
Habibullo <hhalimjonov1997@gmail.com>
2024-09-20 20:22:08 +0900

    fix: refactor pages components

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,5 @@
 import withLayoutMain from "@/libs/components/layout/LayoutHome";
-import { Box, Container, Stack } from "@mui/material";
+import { Stack } from "@mui/material";
 import { brown, green } from "@mui/material/colors";
 import { NextPage } from "next";
 
@@ -7,14 +7,22 @@ const Home: NextPage = () => {
   return (
     <Stack className={"Home-page"}>
       <Stack>
+        <Stack className="container">Trend Properties</Stack>
+      </Stack>
+      <Stack>
         <Stack className="container">Popular Properties</Stack>
       </Stack>
       <Stack>
-        <Stack className="container">Top Agent</Stack>
+        <Stack className="container">Advertisement</Stack>
       </Stack>
       <Stack>
         <Stack className="container">Top Properties</Stack>
       </Stack>
+      <Stack>
+        <Stack className="container">Top Agent</Stack>
+      </Stack>
     </Stack>
   );
 };
+
+export default withLayoutMain(Home)
--- pages/property/index.tsx
+++ pages/property/index.tsx
@@ -1,7 +1,13 @@
 import withLayoutBasic from "@/libs/components/layout/LayoutBasic";
-import { Container, Stack } from "@mui/material";
+import { Stack } from "@mui/material";
 import { NextPage } from "next";
 
 const PropertyList: NextPage = () => {
-  return <Container>Property List</Container>;
+  return (
+    <div style={{ margin: "20px 0" }}>
+      <Stack className="container">Property List</Stack>
+    </div>
+  );
 };
+
+export default withLayoutBasic(PropertyList);
